fffe8213d0ad971439d745f495c4f9757e6b2802
Pavel <hatifnatts@gmail.com>
2015-12-03 14:47:39 +0800

    3dec

--- index.php
+++ index.php
@@ -1,42 +1,128 @@
 <?php include($_SERVER['DOCUMENT_ROOT']. '/templates/meta.html'); ?>
 
 <body class="product-board">
-	<!--<div id="page-preloader"><span class="spinner"></span></div><!--page-preloader-->
+	<div id="page-preloader"><span class="spinner"></span></div><!--page-preloader-->
 	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/nav.html'); ?>
-
-	<!--<div class="container hidden-xs hidden-sm">
+	
+	<div class="jumbotron">
+		<div class="container">
+			<div class="col-sm-7">
+				<h1>Мы охотимся за вкусными <a href="catalog.php">офферами!</a></h1>
+				<p>Мобильная партнёрская сеть, предоставляющая технические и организационные возможности по проведению<br> рекламных кампаний с оплатой за результат.</p>
+				<p><a class="btn btn-primary btn-lg">Начать</a></p>
+			</div>
+			<div class="col-sm-5 text-right">
+				<img class="img-responsive hidden-xs" src="img/mainimg.png" alt="mainimg" />
+			</div>
+		</div><!--container-->
+	</div>
+	
+	
+	<div class="container NewsContainer">
 		<div class="row">
-			<div class="PopInfo inIndex clearfix ">
-				<div class="PopBlock col-md-3"  data-container="body" data-toggle="popover" data-placement="bottom" data-content="Несколько способов доставки">
-					<div class="TryOn"></div>
+				<div class="col-sm-9">
+					
+					<div class="PopInfo inIndex clearfix">
+					<div class="row">
+					<div class="col-md-6">
+						<div class="PopBlock"  >
+					<div class="Exs"></div>
 					
 					
 					<p><b>ЭКСКЛЮЗИВНЫЕ ОФФЕРЫ</b>Офферы от крупнейших рекламодателей. Гарантии максимальных выплат для  мобильного трафика.</p>
 					
- 				</div>
- 				
- 				<div class="PopBlock col-md-3"  data-container="body" data-toggle="popover" data-placement="bottom" data-content="Несколько способов доставки">
-					<div class="Delivery"></div>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-6-->
+					<div class="col-md-6">
+					<div class="PopBlock ">
+					<div class="Manager"></div>
 					
 					<p><b>ПЕРСОНАЛЬНЫЙ МЕНЕДЖЕР</b>Консультации нашего специалиста по арбитражу. Помощь с выбором оффера, подготовко промо-материалов</p>
- 				</div>
- 				
- 				<div class="PopBlock col-md-3"  data-container="body" data-toggle="popover" data-placement="bottom" data-content="Несколько способов доставки">
-					<div class="EasyPay"></div>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-6-->
+					</div><!--row-->
+					<div class="row">
+					<div class="col-md-6">
+					<div class="PopBlock ">
+					<div class="Stat"></div>
 					
 					<p><b>СТАТИСТИКА В ДЕТАЛЯХ</b>Максимум информации о трафике и лидах в режиме реального времени.</p>
- 				</div>
- 				
- 				<div class="PopBlock col-md-3"  data-container="body" data-toggle="popover" data-placement="bottom" data-content="Несколько способов доставки">
-					<div class="Phone"></div>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-6-->
+					
+					<div class="col-md-6">
+					<div class="PopBlock ">
+					<div class="Pay"></div>
 					
 					<p><b>МОМЕНТАЛЬНЫЕ ВЫПЛАТЫ</b>Вывод средств без ограничений по суммам в удобное вам время и через любые платежные системы.</p>
- 				</div>
- 				
-			</div>
-		</div>	
+ 				</div><!--PopBlock-->
+					</div><!--col-md-6-->
+					</div>
+			</div><!--PopInfo-->
+					
+				</div><!--col-sm-9-->
+				<div class="col-sm-3 NewsBlock">
+					
+						<h4>Новости</h4>
+						
+						<div class="News">
+							<div class="row">
+							<div class="col-md-3">
+								<div class="Date">
+								22 <span>окт</span>
+								</div>	
+							</div><!--col-md-3-->
+							<div class="col-md-9">
+								<a href="#">
+								<span class="New">новый оффер</span>
+								Средство для увеличения члена Титан гель
+								</a>
+							</div><!--col-md-9-->
+							</div><!--row-->
+						</div><!--News-->
+						
+						<div class="News">
+							<div class="row">
+							<div class="col-md-3">
+								<div class="Date">
+								22 <span>окт</span>
+								</div>	
+							</div><!--col-md-3-->
+							<div class="col-md-9">
+								<a href="#">
+								<span class="New">новый оффер</span>
+								Приложение livegames
+								</a>
+							</div><!--col-md-9-->
+							</div><!--row-->
+						</div><!--News-->
+						
+						<div class="News">
+							<div class="row">
+							<div class="col-md-3">
+								<div class="Date">
+								22 <span>окт</span>
+								</div>	
+							</div><!--col-md-3-->
+							<div class="col-md-9">
+								<a href="#">
+								<span class="New">новый оффер</span>
+								Билайн Локатор. Целевая конверсия – Ребилл абонента (факт списания денег со счета абонента)
+								</a>
+							</div><!--col-md-9-->
+							</div><!--row-->
+						</div><!--News-->
+						
+				</div><!--NewsBlock-->
+		</div><!--row-->	
 	</div><!--container-->
 	
+	<div class="container Pacman">
+		
+			<img class="img-responsive" src="img/pacmaneat.png" alt="pacmaneat" width="756" height="125" />
+		
+	</div>
+	<div class="White">
 	<div class="container hidden-xs">
 		<div id="radioBtn" class="btn-group board-filters clearfix">
 			
@@ -47,23 +133,11 @@
 	</div>
 				
 	<div id="product-board" class="clearfix">
-	
-	
-	
-		<!--<div class="product large cta">
-			<div class="content">
-				<h3>Новые поступления!</h3>
-				<p>Июль - пожалуй, самый прияный и ожидаемый летний месяц, поэтому мы подготовили для тебя коллекцию, наполненную воздушными платьями.</p>
-			</div>
-			<a href="collection.php" class="btn btn-bottom">Показать</a>	
-		</div>-->
-		<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/item.html'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/item.html'); ?>
 	</div> <!-- //end product-board -->
-	
-	
-	
+	</div><!--White-->
 	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/modal.html'); ?>
-	
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/subscribe.html'); ?>
 	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/footer.html'); ?>
 			
 </body>
---
+++ web.php
@@ -0,0 +1,189 @@
+<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/meta.html'); ?>
+
+<body class="product-board">
+	
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/nav.html'); ?>
+	
+	
+	<div class="container Bigtitle">
+		<div class="row">
+			<div class="col-sm-12 text-center">
+					<h1>Наша мобильная CPA сеть - развитие вашего бизнеса.</h1>
+				</div><!--col-sm-12-->
+		</div>
+	</div>
+	
+	<div class="container NewsContainer">
+		<div class="row">
+				
+				<div class="col-sm-12">
+					
+					<div class="PopInfo inIndex clearfix">
+					<div class="row">
+						<div class="col-md-4">
+						<div class="PopBlock"  >
+					<div class="Exs"></div>
+					
+					
+					<p><b>ЭКСКЛЮЗИВНЫЕ ОФФЕРЫ</b>Офферы от крупнейших рекламодателей. Гарантии максимальных выплат для  мобильного трафика.</p>
+					
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+						<div class="col-md-4">
+					<div class="PopBlock ">
+					<div class="Manager"></div>
+					
+					<p><b>ПЕРСОНАЛЬНЫЙ МЕНЕДЖЕР</b>Консультации нашего специалиста по арбитражу. Помощь с выбором оффера, подготовко промо-материалов</p>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+						<div class="col-md-4">
+					<div class="PopBlock ">
+					<div class="Manager"></div>
+					
+					<p><b>ПЕРСОНАЛЬНЫЙ МЕНЕДЖЕР</b>Консультации нашего специалиста по арбитражу. Помощь с выбором оффера, подготовко промо-материалов</p>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+					</div><!--row-->
+					<div class="row">
+						<div class="col-md-4">
+					<div class="PopBlock ">
+					<div class="Stat"></div>
+					
+					<p><b>СТАТИСТИКА В ДЕТАЛЯХ</b>Максимум информации о трафике и лидах в режиме реального времени.</p>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+						<div class="col-md-4">
+					<div class="PopBlock ">
+					<div class="Stat"></div>
+					
+					<p><b>СТАТИСТИКА В ДЕТАЛЯХ</b>Максимум информации о трафике и лидах в режиме реального времени.</p>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+						<div class="col-md-4">
+					<div class="PopBlock ">
+					<div class="Pay"></div>
+					
+					<p><b>МОМЕНТАЛЬНЫЕ ВЫПЛАТЫ</b>Вывод средств без ограничений по суммам в удобное вам время и через любые платежные системы.</p>
+ 				</div><!--PopBlock-->
+					</div><!--col-md-4-->
+					</div><!--row-->
+			</div><!--PopInfo-->
+					
+				</div><!--col-sm-9-->
+				
+		</div><!--row-->	
+	</div><!--container-->
+	
+	<div class="container Pacman">
+		
+			<img class="img-responsive" src="img/pacmaneat.png" alt="pacmaneat" width="756" height="125" />
+		
+	</div>
+	<div class="White">
+	
+	
+	</div><!--White-->
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<form class="form-horizontal">
+            <fieldset>
+            <!-- Sign Up Form -->
+            <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="Email">Ваше имя:</label>
+              <div class="controls">
+              	<span class="glyphicon glyphicon-asterisk"></span>
+                <input id="Name" name="Name" class="form-control" type="text" placeholder="Введите имя" class="input-large" required="">
+              </div>
+            </div>
+            
+            <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="Email">Ваш Email:</label>
+              <div class="controls">
+              	<span class="glyphicon glyphicon-asterisk"></span>
+                <input id="Email" name="Email" class="form-control" type="text" placeholder="Введите e-mail" class="input-large" required="">
+              </div>
+            </div>
+            
+                        
+            <!-- Password input-->
+            <div class="control-group">
+              <label class="control-label" for="password">Пароль (не менее 6 символов. Должен включать заглавные буквы и символы)</label>
+              <div class="controls">
+              	<span class="glyphicon glyphicon-asterisk"></span>
+                <input id="password" name="password" class="form-control" type="password" placeholder="" class="input-large" required="">
+              
+              </div>
+            </div>
+            
+            <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="reenterpassword">Повторите пароль:</label>
+              <div class="controls">
+              	<span class="glyphicon glyphicon-asterisk"></span>
+                <input id="reenterpassword" class="form-control" name="reenterpassword" type="password" placeholder="" class="input-large" required="">
+              </div>
+            </div>
+            
+            
+           <div class="GreyArea">
+           
+           	 <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="reenterpassword">Skype/icq/Телефон:</label>
+              <div class="controls">
+                <input id="Phone" class="form-control" name="Phone" type="text" placeholder="" class="input-large" >
+              </div>
+            </div>
+            
+            <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="reenterpassword">Кошелек WMR:</label>
+              <div class="controls">
+                <input id="Pay" class="form-control" name="Pay" type="text" placeholder="" class="input-large" >
+              </div>
+            </div>
+            
+            <!-- Text input-->
+            <div class="control-group">
+              <label class="control-label" for="reenterpassword">Промо-код <span class="Question" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Время на которое пользователь будет привязан к сессии">?</span></label>
+              <div class="controls">
+              	
+                <input id="Code" class="form-control" name="Code" type="text" placeholder="" class="input-large" >
+              </div>
+            </div>
+           
+           </div><!--GreyArea-->
+            
+            <!-- Button -->
+            <div class="control-group">
+              <label class="control-label" for="confirmsignup"></label>
+              
+              	<p class="req"><sup><span class="glyphicon glyphicon-asterisk"></span></sup> поля обязательные для заполнения</p>
+              	<div class="checkbox">
+	              	<label>
+	              	  <input type="checkbox" value="">
+	              	  Я согласен(a) с <a href="#">правилами площадки</a>
+	              	</label>
+	              </div>
+	              <br>
+                <button id="confirmsignup" name="confirmsignup" class="btn btn-success">Зарегистрироваться</button>
+              
+            </div>
+            </fieldset>
+            </form>
+			</div><!--col-md-6-->
+			<div class="col-md-6">
+				
+			</div><!--col-md-6-->
+		</div><!--row-->
+	</div><!--container-->
+	
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/modal.html'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT']. '/templates/footer.html'); ?>
+			
+</body>
+</html>
